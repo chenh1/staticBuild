6dc4c935670847991019fb80b0fefaad8004854b
Howard Chen <chenh0886@gmail.com>
2017-06-20 23:41:36 -0700

    Piece work items off into stateless functional

--- src/components/common/WorkNav.js
+++ src/components/common/WorkNav.js
@@ -1,57 +1,60 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
+import WorkItem from './WorkItem';
 
 class WorkNav extends React.Component{
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            contents: [
+                {
+                    path: "amma",
+                    className: "amma",
+                    imageSrc: require("../../assets/amma-main.png"),
+                    details: "AMMA",
+                    description: "Helping asthma sufferers manage and monitor their illness day to day"
+                },
+                {
+                    path: "dignityMeals",
+                    className: "dignity-meals",
+                    imageSrc: require("../../assets/dignity-main.png"),
+                    details: "Dignity Meals",
+                    description: "A food rescue and redistribution app focused on reducing hunger and food waste"
+                },
+                {
+                    path: "abundantLife",
+                    className: "abundant-life",
+                    imageSrc: require("../../assets/alife.png"),
+                    details: "Abundant Life",
+                    description: "Coming Soon!"
+                },
+                {
+                    path: "http://cargocollective.com/tracylin",
+                    className: "graphic-design ",
+                    imageSrc: require("../../assets/graphic-design.png"),
+                    details: "Graphic Design",
+                    description: "Select visual design projects that I've produced in the past years"
+                }
+            ]
+        }
+    }
+
     render() {
         return(
             <div className="work-content">
-                <Link to="amma" className="amma portfolio-item page-link">
-                    <div className="image">
-                        <img src={require("../../assets/amma-main.png")} alt="AMMA"></img>
-                    </div>
-                    <div className="info">
-                        <h3 className="header-three">AMMA</h3>
-                        <p className="paragraph-one">
-                            Helping asthma sufferers manage and monitor their illness day to day
-                        </p>
-                    </div>
-                </Link>
-
-                <Link to="dignityMeals" className="dignity-meals portfolio-item page-link">
-                    <div className="image">
-                        <img src={require("../../assets/dignity-main.png")} alt="Dignity Meals"></img>
-                    </div>
-                    <div className="info">
-                        <h3 className="header-three">Dignity Meals</h3>
-                        <p className="paragraph-one">
-                            A food rescue and redistribution app focused on reducing hunger and food waste
-                        </p>
-                    </div>
-                </Link>
-
-                <Link to="abundantLife" className="abundant-life portfolio-item page-link">
-                    <div className="image">
-                        <img src={require("../../assets/alife.png")} alt="Abundant Life"></img>
-                    </div>
-                    <div className="info">
-                        <h3 className="header-three">Abundant Life</h3>
-                        <p className="paragraph-one">
-                            Coming soon!
-                        </p>
-                    </div>
-                </Link>
-
-                <Link to="http://cargocollective.com/tracylin" className="graphic-design portfolio-item page-link">
-                    <div className="image">
-                        <img src={require("../../assets/graphic-design.png")} alt="Graphic Design"></img>
-                    </div>
-                    <div className="info">
-                        <h3 className="header-three">Graphic Design</h3>
-                        <p className="paragraph-one">
-                            Select visual design projects that I've produced in the past years
-                        </p>
-                    </div>
-                </Link>
+                {this.state.contents.map(workItem => {
+                    return (
+                        <WorkItem
+                            key={workItem.path}
+                            path={workItem.path}
+                            uniqueClass={workItem.className}
+                            imageSrc={workItem.imageSrc}
+                            details={workItem.details}
+                            description={workItem.description}
+                        />
+                    );
+                })}
             </div>
         );
     }
