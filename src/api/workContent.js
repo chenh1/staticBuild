7221c37ebbe3e2584a72e7a010e2c4c172da41e3
Howard Chen <chenh0886@gmail.com>
2017-08-05 21:55:00 -0700

    Update path

--- src/api/workContent.js
+++ src/api/workContent.js
@@ -1,27 +1,31 @@
 const workContent = [
     {
-        "imageSrc": require("../assets/amma-main.png"),
-        "alt": "AMMA",
-        "subHeader": "AMMA",
-        "description": "Helping asthma sufferers manage and monitor their illness day to day"
+        path: "amma",
+        imageSrc: require("../assets/amma-main.png"),
+        alt: "AMMA",
+        subHeader: "AMMA",
+        description: "Helping asthma sufferers manage and monitor their illness day to day"
     },
     {
-        "imageSrc": require("../assets/dignity-main.png"),
-        "alt": "Dignity Meals",
-        "subHeader": "Dignity Meals",
-        "description": "A food rescue and redistribution app focused on reducing hunger and food waste"
+        path: "dignityMeals",
+        imageSrc: require("../assets/dignity-main.png"),
+        alt: "Dignity Meals",
+        subHeader: "Dignity Meals",
+        description: "A food rescue and redistribution app focused on reducing hunger and food waste"
     },
     {
-        "imageSrc": require("../assets/alife.png"),
-        "alt": "Abundant Life",
-        "subHeader": "Abundant Life",
-        "description": "Coming Soon!"
+        path: "abundantLife",
+        imageSrc: require("../assets/alife.png"),
+        alt: "Abundant Life",
+        subHeader: "Abundant Life",
+        description: "Coming Soon!"
     },
     {
-        "imageSrc": require("../assets/graphic-design.png"),
-        "alt": "Graphic Design",
-        "subHeader": "Graphic Design",
-        "description": "Select visual design projects that I've produced in the past years"
+        path: "http://cargocollective.com/tracylin",
+        imageSrc: require("../assets/graphic-design.png"),
+        alt: "Graphic Design",
+        subHeader: "Graphic Design",
+        description: "Select visual design projects that I've produced in the past years"
     }
 ];
 
--- src/components/organisms/WorkItem.js
+++ src/components/organisms/WorkItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PortfolioImage from '../molecules/PortfolioImage';
 import PortfolioInfo from '../molecules/PortfolioInfo';
+import { Link, IndexLink } from 'react-router';
 
 const WorkItem = ({data}) => {
     return (
-        <div className="portfolio-item page-link">
+        <Link to={data.path} className="portfolio-item page-link">
             <PortfolioImage imageSrc={data.imageSrc} alt={data.alt}/>
             <PortfolioInfo subHeaderText={data.subHeader} paragraphText={data.description}/>
-        </div>
+        </Link>
     );
 };
 
